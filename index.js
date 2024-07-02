239af313bbac29ce5dc3fab2c3d10ff088f46571
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-07-02 09:20:00 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin (record) {
+    const winRecord = record.find(entry => entry.result === "W");
+    if (winRecord) {
+        return winRecord.year;
+    } else {
+        return undefined;
+    }
+}
